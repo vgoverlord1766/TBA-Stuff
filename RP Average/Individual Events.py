fcef3153efbedc03f9a5031089ee09b692740e19
vgoverlord1766 <jgomez@thinkbox.us>
2024-01-16 15:26:14 -0500

    Added districts stuff

--- RP Average/Individual Events.py
+++ RP Average/Individual Events.py
@@ -21,7 +21,6 @@ qm_match_counter = 0
 for match in matches:
     if "qm" in match:
         qm_match_counter += 1
-        print(qm_match_counter)
         match_response = requests.get("https://www.thebluealliance.com/api/v3/match/" + match, params=parameters)
         match_data = json.loads(match_response.content)
         qm_rp_total_counter += match_data["score_breakdown"]["blue"]["rp"] + match_data["score_breakdown"]["red"]["rp"]
