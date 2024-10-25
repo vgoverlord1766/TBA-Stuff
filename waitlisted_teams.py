591c0c9f612f6d0a6e00cc6f316472ba865e2a75
vgoverlord1766 <jgomez@thinkbox.us>
2024-10-25 10:14:49 -0400

    t

---
+++ waitlisted_teams.py
@@ -0,0 +1,29 @@
+import requests
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+AUTH_KEY = os.getenv('AUTH_KEY')
+parameters = {
+    "X-TBA-Auth-Key": AUTH_KEY
+}
+
+teams_responses = requests.get("https://www.thebluealliance.com/api/v3/district/2024ne/teams/keys",
+                                params=parameters)
+teams = json.loads(teams_responses.content)
+
+teams_with_one_event = []
+i=0
+
+for team in teams:
+    team_event_responses = requests.get("https://www.thebluealliance.com/api/v3/team/" + team + "/events/2025/keys",
+                                   params=parameters)
+    team_event = json.loads(team_event_responses.content)
+    if len(team_event) is not 1:
+        teams_with_one_event.append(team)
+
+    i += 1
+    print(i)
+
+print(teams_with_one_event)
